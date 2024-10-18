a6027840d6ec161119f165d52457fb864893183c
amol <amlo1232001@gmail.com>
2024-10-18 15:12:04 +0530

    feat: add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import PublicLayout from "@/components/custom/layout/public";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <PublicLayout>
       <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        <section className="min-h-[calc(100vh_-_theme(spacing.64))] flex flex-col justify-center lg:items-center">
+          <h1 className="text-4xl font-bold tracking-tight drop-shadow-md">
+            Effortless Rentals, Seamless Management
+          </h1>
+          <p className="mt-4 text-lg text-muted-foreground">
+            Simplify your rental process with our innovative SaaS solution
+          </p>
+          <Button asChild className="mt-4 max-w-32">
+            <Link href="/sign-in">Get Started</Link>
+          </Button>
+        </section>
       </main>
     </PublicLayout>
   );
