b89b62f8f290194b5005ce27a1d3b406d85c5c46
amol <amlo1232001@gmail.com>
2024-10-19 14:59:01 +0530

    feat: add clerk

---
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,12 @@
+import PublicLayout from "@/components/custom/layout/public";
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <PublicLayout>
+      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col items-center gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+        <SignIn />
+      </main>
+    </PublicLayout>
+  );
+}
