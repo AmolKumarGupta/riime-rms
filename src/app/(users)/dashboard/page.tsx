eb7737951756f93224800b5797984adca369e3e4
amol <amlo1232001@gmail.com>
2025-01-09 16:04:28 +0530

    feat: add recent activity section with skeleton loading to dashboard

--- src/app/(users)/dashboard/page.tsx
+++ src/app/(users)/dashboard/page.tsx
@@ -1,4 +1,6 @@
 import StatCard from "@/components/custom/stat-card";
+import { Separator } from "@/components/ui/separator";
+import { Skeleton } from "@/components/ui/skeleton";
 import { property, tenant } from "@/db/facades";
 import { auth } from "@clerk/nextjs/server";
 import { DollarSign, HomeIcon, UserIcon } from "lucide-react";
@@ -35,6 +37,18 @@ export default async function Page() {
           icon={<DollarSign className="h-4 w-4 text-yellow-500" />}
           content="$ 12,345.99"
         />
+      </section>
+
+      <section className="my-8 px-2">
+        <header>
+          <h2 className="text-2xl font-bold">Recent Activity</h2>
+        </header>
+
+        <Separator className="my-4" />
+
+        <div>
+          <Skeleton className="w-100 h-48" />
+        </div>
       </section>
     </>
   );
